788bfe1243a3e7cf4bbb41afa021b0f0dd451fec
Neil <643241432@qq.com>
2020-02-07 23:10:51 +0800

    Initial Commit

---
+++ QPikachu/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  QPikachu
+//
+//  Created by 张雨飞 on 7/02/20.
+//  Copyright © 2020 张雨飞. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ QPikachu/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  QPikachu
+//
+//  Created by 张雨飞 on 7/02/20.
+//  Copyright © 2020 张雨飞. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
