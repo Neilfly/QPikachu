b166ef42ddf672ceabb5144f5b1f502abb9d4d42
Neil <643241432@qq.com>
2020-02-21 21:59:07 +0800

    做了自适应大小的圆形头像图和背景图

--- QPikachu/Controllers/SecondMainViewController.swift
+++ QPikachu/Controllers/SecondMainViewController.swift
@@ -10,21 +10,25 @@ import UIKit
 
 class SecondMainViewController: UIViewController {
     
+    @IBOutlet weak var headImage: UIImageView!
+    @IBOutlet weak var backGroundImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        backGroundImage.alpha = 0.4
+        self.view.layoutIfNeeded()
+        let headImageLayer: CALayer = headImage.layer
+        headImageLayer.masksToBounds = true
+        headImageLayer.cornerRadius = headImage.frame.width / 2
+        headImageLayer.borderWidth = 5
+        headImageLayer.borderColor = #colorLiteral(red: 0.4882740378, green: 0.6144382954, blue: 0.8381537795, alpha: 1)
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
