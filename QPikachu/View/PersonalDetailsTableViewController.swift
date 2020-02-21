ff31c500e156a0a3eb38aa2031db7b501364fac8
Neil <643241432@qq.com>
2020-02-21 22:57:34 +0800

    添加了用户名自适应label，以及个人信息下方的container view里的static tableview

---
+++ QPikachu/View/PersonalDetailsTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PersonalDetailsTableViewController.swift
+//  QPikachu
+//
+//  Created by 张雨飞 on 21/02/20.
+//  Copyright © 2020 张雨飞. All rights reserved.
+//
+
+import UIKit
+
+class PersonalDetailsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
